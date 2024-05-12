d0625fa3ca79ae7654a789ba9b9093fc6cd33da7
zidan-bsd <bsdbisnis@gmail.com>
2024-05-12 11:55:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import quotesData from "./datas/quotes.json";
+
+import "./styles/Global.sass";
 
 function App() {
+  const [quote, setQuote] = useState({});
+
+  const generateQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotesData.length);
+    setQuote(quotesData[randomIndex]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote">
+      <p className="quote-title">Random Quote</p>
+      {quote.quote && (
+        <div className="quote-quote">
+          <p className="quote-quote-text">{quote.quote}</p>
+          <p className="quote-quote-author">- {quote.author}</p>
+        </div>
+      )}
+      <button className="quote-button" onClick={generateQuote}>
+        Generate
+      </button>
     </div>
   );
 }
